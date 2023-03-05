e2aa857f3ef718c4c4436e8db1f390db542d1152
Oleg Kropiviansky <oleg.kropiviansky@gmail.com>
2023-03-05 19:49:24 +0200

    ref: Searchbar and App

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,14 @@
-import { Component } from 'react';
+import { useState, useEffect } from 'react';
 import axios from 'axios';
 import Button from './Button/Button';
 import Loader from './Loader/Loader';
-import { Searchbar } from './Searchbar/Searchbar';
+import Searchbar from './Searchbar/Searchbar';
 import ImageGallery from './ImageGallery/ImageGallery';
 import Modal from './Modal/Modal';
 import style from './App.module.css';
 
 const KEY = '33348757-d3bd4442be84b253a20cc414a';
-
+const itemsPerPage = 12;
 const Status = {
   LOADING: 'loading',
   IDLE: 'idle',
@@ -16,103 +16,160 @@ const Status = {
   SUCCESS: 'success',
 };
 
-export class App extends Component {
-  state = {
-    searchQuery: '',
-    images: [],
-    page: 1,
-    perPage: 12,
-    status: Status.IDLE,
-    showModal: false,
-    selectedImageId: '',
-    totalImages: 0,
+const App = () => {
+  // state = {
+  //   searchQuery: '',
+  //   images: [],
+  //   page: 1,
+  //   perPage: 12,
+  //   status: Status.IDLE,
+  //   showModal: false,
+  //   selectedImageId: '',
+  //   totalImages: 0,
+  // };
+
+  // componentDidMount() {
+  //   this.fetchImages();
+  // }
+
+  // componentDidUpdate(prevProps, prevState) {
+  //   if (
+  //     prevState.searchQuery !== this.state.searchQuery ||
+  //     prevState.page !== this.state.page
+  //   ) {
+  //     this.fetchImages();
+  //   }
+  // }
+
+  // fetchImages = async () => {
+  //   if (!this.state.searchQuery) {
+  //     return;
+  //   }
+
+  //   try {
+  //     this.setState({ status: Status.LOADING });
+  //     const result = await axios.get(
+  //       `https://pixabay.com/api/?q=${this.state.searchQuery}&page=${this.state.page}&key=${KEY}&image_type=photo&orientation=horizontal&per_page=${this.state.perPage}`
+  //     );
+  //     this.setState({
+  //       images: [...this.state.images, ...result.data.hits],
+  //       status: Status.SUCCESS,
+  //       totalImages: result.data.totalHits,
+  //     });
+  //   } catch (error) {
+  //     console.error(error);
+  //     this.setState({ status: Status.ERROR });
+  //   }
+  // };
+
+  // handleFormSubmit = search => {
+  //   if (search === this.state.searchQuery) {
+  //     alert(
+  //       'this request has already been processed. Please enter new request'
+  //     );
+  //     return;
+  //   }
+  //   this.setState({ searchQuery: search, images: [], page: 1 });
+  // };
+
+  // changePage = () => {
+  //   this.setState({ page: this.state.page + 1 });
+  // };
+
+  // toggleModal = imageId => {
+  //   this.setState(({ showModal }) => ({
+  //     showModal: !showModal,
+  //     selectedImageId: imageId || '',
+  //   }));
+  // };
+
+  // render() {
+  //   const {
+  //     images,
+  //     status,
+  //     showModal,
+  //     selectedImageId,
+  //     searchQuery,
+  //     totalImages,
+  //   } = this.state;
+  //   const hasImages = !!images.length;
+  //   const hasMoreImages = images.length < totalImages;
+  //   const selectedImage = images.find(image => {
+  //     return image.id === selectedImageId;
+  //   });
+  const [searchQuery, setSearchQuery] = useState('');
+  const [images, setImages] = useState([]);
+  const [page, setPage] = useState(1);
+  const [status, setStatus] = useState(Status.IDLE);
+  const [selectedImageId, setSelectedImageId] = useState('');
+  const [totalImages, setTotalImages] = useState(0);
+
+  const fetchImages = async (search, page = 1) => {
+    const result = await axios.get(
+      `https://pixabay.com/api/?q=${search}&page=${page}&key=${KEY}&image_type=photo&orientation=horizontal&per_page=${itemsPerPage}`
+    );
+    return { images: result.data.hits, totalImages: result.data.totalHits };
   };
 
-  componentDidMount() {
-    this.fetchImages();
-  }
-
-  componentDidUpdate(prevProps, prevState) {
-    if (
-      prevState.searchQuery !== this.state.searchQuery ||
-      prevState.page !== this.state.page
-    ) {
-      this.fetchImages();
-    }
-  }
-
-  fetchImages = async () => {
-    if (!this.state.searchQuery) {
+  useEffect(() => {
+    if (!searchQuery) {
       return;
     }
+    const saveImages = async () => {
+      try {
+        setStatus(Status.LOADING);
+        const { images: newImages, totalImages: fetchTotalImages } =
+          await fetchImages(searchQuery, page);
+        setImages(oldImages => [...oldImages, ...newImages]);
+        setStatus(Status.SUCCESS);
+        setTotalImages(fetchTotalImages);
+      } catch (error) {
+        alert(error);
+        setStatus(Status.ERROR);
+      }
+    };
+    saveImages();
+  }, [page, searchQuery]);
 
-    try {
-      this.setState({ status: Status.LOADING });
-      const result = await axios.get(
-        `https://pixabay.com/api/?q=${this.state.searchQuery}&page=${this.state.page}&key=${KEY}&image_type=photo&orientation=horizontal&per_page=${this.state.perPage}`
-      );
-      this.setState({
-        images: [...this.state.images, ...result.data.hits],
-        status: Status.SUCCESS,
-        totalImages: result.data.totalHits,
-      });
-    } catch (error) {
-      console.error(error);
-      this.setState({ status: Status.ERROR });
-    }
-  };
-
-  handleFormSubmit = search => {
-    if (search === this.state.searchQuery) {
-      alert(
+  const handleFormSubmit = search => {
+    if (search === searchQuery) {
+      alert.info(
         'this request has already been processed. Please enter new request'
       );
       return;
     }
-    this.setState({ searchQuery: search, images: [], page: 1 });
+    setSearchQuery(search);
+    setPage(1);
+    setImages([]);
+    setTotalImages(0);
   };
 
-  changePage = () => {
-    this.setState({ page: this.state.page + 1 });
+  const loadMore = () => {
+    setPage(page + 1);
   };
 
-  toggleModal = imageId => {
-    this.setState(({ showModal }) => ({
-      showModal: !showModal,
-      selectedImageId: imageId || '',
-    }));
+  const toggleModal = imageId => {
+    setSelectedImageId(imageId || '');
   };
 
-  render() {
-    const {
-      images,
-      status,
-      showModal,
-      selectedImageId,
-      searchQuery,
-      totalImages,
-    } = this.state;
-    const hasImages = !!images.length;
-    const hasMoreImages = images.length < totalImages;
-    const selectedImage = images.find(image => {
-      return image.id === selectedImageId;
-    });
-    return (
-      <div className={style.App}>
-        {showModal && selectedImage && (
-          <Modal onClose={this.toggleModal} image={selectedImage} />
-        )}
-        <Searchbar onSubmit={this.handleFormSubmit} searchQuery={searchQuery} />
-        <ImageGallery
-          images={this.state.images}
-          onImageItemClick={this.toggleModal}
-        />
-        {!hasImages && 'Please enter a search word'}
-        {hasImages && hasMoreImages && status === Status.SUCCESS && (
-          <Button onClick={this.changePage} />
-        )}
-        {status === Status.LOADING && <Loader />}
-      </div>
-    );
-  }
-}
+  const hasImages = !!images.length;
+  const hasMoreImages = images.length < totalImages;
+  const selectedImage = images.find(image => {
+    return image.id === selectedImageId;
+  });
+
+  return (
+    <div className={style.App}>
+      {selectedImage && <Modal onClose={toggleModal} image={selectedImage} />}
+      <Searchbar onSubmit={handleFormSubmit} searchQuery={searchQuery} />
+      <ImageGallery images={images} onImageItemClick={toggleModal} />
+      {!hasImages && 'Please enter a search word'}
+      {hasImages && hasMoreImages && status === Status.SUCCESS && (
+        <Button onClick={loadMore} />
+      )}
+      {status === Status.LOADING && <Loader />}
+    </div>
+  );
+};
+
+export default App;
--- src/components/Searchbar/Searchbar.jsx
+++ src/components/Searchbar/Searchbar.jsx
@@ -1,50 +1,48 @@
-import { Component } from 'react';
+import { useState } from 'react';
 import { IoMdSearch } from 'react-icons/io';
 import PropTypes from 'prop-types';
 import style from './Searchbar.module.css';
 
-export class Searchbar extends Component {
-  state = {
-    search: '',
+const Searchbar = ({ onSubmit, searchQuery }) => {
+  const [search, setSearch] = useState('');
+
+  const handleInputChange = event => {
+    setSearch(event.currentTarget.value.toLowerCase());
   };
 
-  handleInputChange = event => {
-    this.setState({ search: event.currentTarget.value.toLowerCase() });
-  };
-
-  handleSubmit = event => {
+  const handleSubmit = event => {
     event.preventDefault();
-    if (this.state.search.trim() === '') {
+    if (search.trim() === '') {
       alert('Please enter a search word');
       return;
     }
-    this.props.onSubmit(this.state.search);
-    this.setState({ search: '' });
+    onSubmit(search);
+    setSearch('');
   };
 
-  render() {
-    return (
-      <header className={style.Searchbar}>
-        <form className={style.SearchForm} onSubmit={this.handleSubmit}>
-          <button type="submit" className={style.SearchForm__button}>
-            <IoMdSearch className={style.SearchForm__button__icon} />
-          </button>
-          <input
-            className={style.SearchForm__input}
-            type="text"
-            autoComplete="off"
-            autoFocus
-            placeholder={'Search images and photos'}
-            value={this.state.search}
-            onChange={this.handleInputChange}
-          />
-        </form>
-      </header>
-    );
-  }
-}
+  return (
+    <header className={style.Searchbar}>
+      <form className={style.SearchForm} onSubmit={handleSubmit}>
+        <button type="submit" className={style.SearchForm__button}>
+          <IoMdSearch className={style.SearchForm__button__icon} />
+        </button>
+        <input
+          className={style.SearchForm__input}
+          type="text"
+          autoComplete="off"
+          autoFocus
+          placeholder={'Search images and photos'}
+          value={search}
+          onChange={handleInputChange}
+        />
+      </form>
+    </header>
+  );
+};
 
 Searchbar.propTypes = {
   onSubmit: PropTypes.func.isRequired,
   searchQuery: PropTypes.string,
 };
+
+export default Searchbar;
