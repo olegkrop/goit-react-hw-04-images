12877366a21b81a49b9d1da8143feed2c78c7b3e
Oleg Kropiviansky <oleg.kropiviansky@gmail.com>
2023-03-05 19:20:38 +0200

    ref: Loader

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,7 @@
 import { Component } from 'react';
 import axios from 'axios';
 import Button from './Button/Button';
-import { Loader } from './Loader/Loader';
+import Loader from './Loader/Loader';
 import { Searchbar } from './Searchbar/Searchbar';
 import ImageGallery from './ImageGallery/ImageGallery';
 import { Modal } from './Modal/Modal';
